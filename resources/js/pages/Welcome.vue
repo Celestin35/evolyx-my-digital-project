8ad0f7d2729e04b3dc7490e5e76989784d4c2ff6
Célestin Piaton <celestin.piatonpro@gmail.com>
2026-03-12 13:58:17 +0100

    Test de deploiement

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -59,7 +59,7 @@ withDefaults(
                 >
                     <h1 class="mb-1 font-medium">Let's get started</h1>
                     <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">
-                        INES's DESCRIPTION
+                        Dernier test de description
                     </p>
                     <ul class="mb-4 flex flex-col lg:mb-6">
                         <li
